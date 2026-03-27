f53525137b6f3871c22d3aa1de639da1f2ef1762
haochen1492 <KHOH.HAO.CHEN@student.mmu.edu.my>
2026-03-27 09:35:26 +0800

    added header

---
+++ customer/includes/header.php
@@ -0,0 +1,16 @@
+<p>Online Grocery Store</p>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="products.php">Products</a>
+    <a href="Contact.php">Contact</a>
+    <a href="about.php">About Us</a>
+    <a href="cart.php">Cart </a>
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <span>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</span>
+        <a href="logout.php">Logout</a>
+        <a href="orders.php">Order History</a>
+        <a href="profile.php">Profile</a>
+    <?php else: ?>
+        <a href="login.php">Login</a>
+    <?php endif; ?>
+</nav>
