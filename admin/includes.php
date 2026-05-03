e8c763aab6b415f49b557c6f394b8d11dd233327
tanzihui1120-ops <tanzihui1120@gmail.com>
2026-05-03 17:21:00 +0800

    Update includes.php

--- admin/includes.php
+++ admin/includes.php
@@ -1,18 +1,1 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'grocery_admin');
 
-function getDB() {
-    static $conn = null;
-    if ($conn === null) {
-        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $conn->set_charset("utf8");
-    }
-    return $conn;
-}
-?>
