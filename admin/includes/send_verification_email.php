c33de250eadc004580999fd8b6951061c26b38a1
tanzihui1120-ops <tanzihui1120@gmail.com>
2026-06-07 21:07:12 +0800

    Create send_verification_email.php

---
+++ admin/includes/send_verification_email.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+function sendVerificationEmail($email, $username, $token)
+{
+    $verifyLink =
+        "http://localhost/InfinityGrocer/admin/verify.php?token=" . $token;
+
+    $mail = new PHPMailer(true);
+
+    try {
+
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'yourgmail@gmail.com';
+        $mail->Password   = 'your-app-password';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        $mail->setFrom(
+            'yourgmail@gmail.com',
+            'Infinity Grocer'
+        );
+
+        $mail->addAddress($email, $username);
+
+        $mail->isHTML(true);
+
+        $mail->Subject = 'Verify Your Email Address';
+
+        $mail->Body = "
+        <h2>Email Verification</h2>
+
+        <p>Hello {$username},</p>
+
+        <p>Please verify your email address by clicking the button below.</p>
+
+        <p>
+            <a href='{$verifyLink}'
+               style='background:#1e6641;
+                      color:white;
+                      padding:10px 20px;
+                      text-decoration:none;
+                      border-radius:5px;'>
+               Verify Email
+            </a>
+        </p>
+
+        <p>If you did not request this, please ignore this email.</p>
+        ";
+
+        return $mail->send();
+
+    } catch (Exception $e) {
+        return false;
+    }
+}
